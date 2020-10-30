dc3b78449118839fa1502ab22bc84cbdd654c243
Valeria Rosaria Vecchio <valeria.vecchio@btb.gmbh>
2020-10-30 13:48:23 +0100

    replace Stack by ArrayList due to performance

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -9,12 +9,12 @@ public class MergeIntervals {
      * @param givenIntervals
      * @return stack of merged intervals
      */
-    private static Stack<Interval> merge(ArrayList<Interval> givenIntervals) {
+    private static ArrayList<Interval> merge(ArrayList<Interval> givenIntervals) {
         // Create an empty stack of intervals
-        Stack<Interval> stack = new Stack<>();
+        ArrayList<Interval> stack = new ArrayList<Interval>();
 
         // check if the given data has at least one interval
-        if (givenIntervals.isEmpty()) {
+        if (givenIntervals.size() <= 1) {
             return stack;
         }
 
@@ -26,7 +26,7 @@ public class MergeIntervals {
         });
 
         // push first interval to the stack
-        stack.push(givenIntervals.get(0));
+        stack.add(givenIntervals.get(0));
 
         // to increase performance set size of givenIntervals to a variable
         int intervalsSize = givenIntervals.size();
@@ -35,19 +35,19 @@ public class MergeIntervals {
         // after the first is already in the stack
         for (int i = 1; i < intervalsSize; i++) {
             // get interval from stack top
-            Interval top = stack.peek();
+            Interval top = stack.get(stack.size() - 1);
 
             // if current interval is not overlapping with stack top,
             // push it to the stack
             if (top.end < givenIntervals.get(i).start)
-                stack.push(givenIntervals.get(i));
+                stack.add(givenIntervals.get(i));
 
                 // Otherwise update the ending time of top if ending of current
                 // interval is more
             else if (top.end < givenIntervals.get(i).end) {
                 top.end = givenIntervals.get(i).end;
-                stack.pop();
-                stack.push(top);
+                stack.remove(stack.size() - 1);
+                stack.add(top);
             }
         }
         return stack;
@@ -64,14 +64,14 @@ public class MergeIntervals {
 
         System.out.println("Starting Intervals: ");
 
-        //generate e.g. 5 intervals with random values
+        // generate e.g. 5 intervals with random values
         for (int i = 0; i < 5; i++) {
 
             // generate a random number between [0 - 40].
             int random1 = (int) (30.0 * Math.random());
             int random2 = (int) (30.0 * Math.random());
 
-            //check whether which random generated number is smaller
+            // check whether which random generated number is smaller
             if (random1 < random2) {
                 interval = new Interval(random1, random2);
             } else {
@@ -87,14 +87,12 @@ public class MergeIntervals {
 
 
     public static void main(String args[]) {
-        //measure time
+        // measure time
         final long timeStart = System.currentTimeMillis();
 
-        // create a random set of intervals
+        // create a random set of intervals and merge them
         ArrayList<Interval> randomIntervals = generateRandomIntervals();
-
-        Stack<Interval> result;
-        result = merge(randomIntervals);
+        ArrayList<Interval> result = merge(randomIntervals);
 
         System.out.println("\nResult: ");
 
